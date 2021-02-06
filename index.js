2e1ee3ee0869797ac9521bd167efa7f66daee59f
noeulnight <noeulnight.mail@gmail.com>
2021-02-06 09:47:58 +0900

    noice log

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const client = require('discord-rich-presence')('807061207718690837')
 const superagent = require('superagent')
+const packagecode = 'kr.hanjin'
+const packageid = '418694701305'
 
 update()
 setInterval(() => update(), 60000)
 
 async function update () {
-  const res = await superagent.get('https://apis.tracker.delivery/carriers/kr.hanjin/tracks/418694701305')
+  const res = await superagent.get('https://apis.tracker.delivery/carriers/' + packagecode + '/tracks/' + packageid)
   const json = JSON.parse(res.text)
   const packages = json.progresses[json.progresses.length - 1]
   client.updatePresence({ state: packages.status.text, details: packages.description, largeImageKey: 'quest', instance: true })
-  console.log('updated!')
+  console.log('상태 업데이트됨! > ' + packages.description + ' > (' + Date.now() + ')')
 }
